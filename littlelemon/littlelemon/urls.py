5824bc86f3a57b636b09ebc88b4ca0df74e38c21
aulkdev <businessaulk@gmail.com>
2023-01-20 18:53:27 +0100

    added user authentication

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
 from restaurant.views import BookingViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register(r'tables', BookingViewSet)
@@ -24,5 +25,8 @@ router.register(r'tables', BookingViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/menu/', include('restaurant.urls')),
-    path('restaurant/booking/', include(router.urls))
+    path('restaurant/booking/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token)
 ]
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path('', views.index, name="index"),
     path('items/', views.MenuItemView.as_view()),
